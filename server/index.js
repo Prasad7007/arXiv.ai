b4691f0bb600d4ab0551c4c5b294c8dcbc42e1c6
Prasad7007 <prasadagalave0007@gmail.com>
2025-07-11 18:39:36 +0530

    Added cors functionality

--- server/index.js
+++ server/index.js
@@ -3,6 +3,15 @@ const cors = require("cors");
 const rootRouter = require("./routes/index.js")
 
 const app = express();
+
+const allowedOrigins = [`${process.env.FRONTEND_URL}`];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true 
+}));
+
 app.use(express.json());
 
 app.use(cors());
